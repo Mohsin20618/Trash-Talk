549d78367e0e13df3b9f7812e266d27c1fc9c8cc
ghulammurtuza <gmurtuza95@gmail.com>
2024-02-11 14:50:45 +0500

    scrolling with right left buttons added;

--- Assets/Scripts/UI/Shop/ShopPanel.cs
+++ Assets/Scripts/UI/Shop/ShopPanel.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using System;
 using TrashTalk;
+using UnityEngine.UI;
+using System.Collections;
 
 public class ShopPanel : UIPanel
 {
-   public override void Show()
+    public Button leftBtn; public Button rightBtn;
+    public ScrollRect scrollView;
+
+    public float scrollAmount = 0.1f; // Adjust this value for the amount to scroll
+    public float scrollSpeed = 0.5f; // Adjust this value for desired smoothness
+    private bool isScrolling = false;
+
+    private void Start()
+    {
+        // Add listeners to the buttons
+        rightBtn.onClick.AddListener(ScrollRight);
+        leftBtn.onClick.AddListener(ScrollLeft);
+    }
+
+    private void ScrollRight()
+    {
+        if (!isScrolling)
+            StartCoroutine(ScrollSmoothly(Vector2.right));
+    }
+
+    private void ScrollLeft()
+    {
+        if (!isScrolling)
+            StartCoroutine(ScrollSmoothly(Vector2.left));
+    }
+
+    private IEnumerator ScrollSmoothly(Vector2 direction)
+    {
+        isScrolling = true;
+        float elapsedTime = 0f;
+        Vector2 startPosition = scrollView.normalizedPosition;
+        Vector2 endPosition = startPosition + direction * scrollAmount;
+
+        while (elapsedTime < scrollSpeed)
+        {
+            scrollView.normalizedPosition = Vector2.Lerp(startPosition, endPosition, (elapsedTime / scrollSpeed));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        scrollView.normalizedPosition = endPosition;
+        isScrolling = false;
+    }
+
+    public override void Show()
    {
         gameObject.SetActive(true);
    }
@@ -14,6 +60,7 @@ public class ShopPanel : UIPanel
    public override void Hide()
    {
         gameObject.SetActive(false);
+        UIEvents.ShowPanel(Panel.GameSelectPanel);
    }
 
     public override void UpdateData(System.Action<object[]> callBack, params object[] parameters)
@@ -31,6 +78,7 @@ public class ShopPanel : UIPanel
     }
 
     int purchasedCoins = 0;
+
     void PurchaseThroughInApp(int totalCoins, int price, string productID)
     {
   //      InappManager.instance.PurchaseItem(productID, (payload, signature) => {

ea300a4b65b22d2944728fad543ae06ff12c6ec5
ghulammurtuza <64938567+ghulammurtuza@users.noreply.github.com>
2024-02-24 21:30:35 +0500

    room selection card script and game flow with coin selection;

---
+++ Assets/Scripts/UI/Gameplay/RoomSelectionCard.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomSelectionCard : MonoBehaviour
+{   public int entryCoins;
+    public void OnSelectRoom()
+    {
+        if (entryCoins == -1)
+        {
+            entryCoins = (int)PlayerProfile.Player_coins;
+        }
+        Global.coinsRequired = entryCoins;
+
+        print("ROOM SELECTED WITH COINS: " + Global.coinsRequired);
+
+        UIEvents.HidePanel(Panel.TabPanels);
+        UIEvents.ShowPanel(Panel.GameplayPanel);
+
+        //Hunain
+        VoiceManager.instance.EnableDisableVoiceManager();
+        ChatHandler.instance.EnableDisableChatManager(false);
+    }
+}
